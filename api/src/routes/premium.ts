2d3370a8170e180ab6299ebf4c57a0ed46635985
Lorenzo <hello@lorenzo0111.me>
2024-07-19 14:10:03 +0200

    Added stripe backend

--- api/src/routes/premium.ts
+++ api/src/routes/premium.ts
@@ -1,6 +1,98 @@
 import { Hono } from "hono";
-import { plans } from "../constants/premium";
+import type { Stripe } from "stripe";
+import { z } from "zod";
+import type { Variables } from "../constants/context";
+import { getPlan, plans } from "../constants/premium";
+import prisma from "../lib/prisma";
+import { stripe } from "../lib/stripe";
+import { authenticated } from "../middlewares/auth";
+import { zValidator } from "../middlewares/validator";
 
-export const premiumRoute = new Hono().get("/plans", async (ctx) => {
-  return ctx.json(plans);
-});
+export const premiumRoute = new Hono<{ Variables: Variables }>()
+  .get("/plans", async (ctx) => {
+    return ctx.json(plans);
+  })
+  .post(
+    "/subscribe",
+    authenticated,
+    zValidator(
+      "json",
+      z.object({
+        plan: z.string(),
+      }),
+    ),
+    async (ctx) => {
+      const user = ctx.get("user");
+      const body = ctx.req.valid("json");
+      const plan = getPlan(body.plan);
+
+      if (user.plan === plan.id)
+        return ctx.json(
+          {
+            error: "You are already subscribed to this plan",
+          },
+          400,
+        );
+
+      let stripeId = user.stripeId;
+      if (!stripeId) {
+        const customer = await stripe.customers.create({
+          email: user.email,
+          name: user.name,
+        });
+
+        stripeId = customer.id;
+        await prisma.user.update({
+          where: { id: user.id },
+          data: { stripeId },
+        });
+      }
+
+      if (plan.price === 0) {
+        const customer = await stripe.customers.retrieve(stripeId);
+        if (!customer.deleted) {
+          for (const item of customer.subscriptions?.data ?? []) {
+            if (item.status !== "active") continue;
+
+            await stripe.subscriptions.cancel(item.id);
+          }
+        }
+
+        await prisma.user.update({
+          where: { id: user.id },
+          data: { plan: plan.id },
+        });
+
+        return ctx.json({ success: true });
+      }
+
+      const items = await stripe.products.search({
+        query: `metadata["id"]:"${plan.id}"`,
+      });
+      console.log(items);
+      const item = items.data.find((item) =>
+        process.env.NODE_ENV === "production" ? item.livemode : !item.livemode,
+      );
+      if (!item) return ctx.json({ error: "Plan not found" }, 400);
+
+      const subscription = await stripe.subscriptions.create({
+        customer: stripeId,
+        items: [
+          {
+            price: item.default_price as string,
+          },
+        ],
+        payment_behavior: "default_incomplete",
+        payment_settings: { save_default_payment_method: "on_subscription" },
+        expand: ["latest_invoice.payment_intent"],
+      });
+
+      return ctx.json({
+        subscriptionId: subscription.id,
+        clientSecret: (
+          (subscription.latest_invoice as Stripe.Invoice)
+            ?.payment_intent as Stripe.PaymentIntent
+        )?.client_secret,
+      });
+    },
+  );
--- app/app/(auth)/premium.tsx
+++ app/app/(auth)/premium.tsx
@@ -4,6 +4,7 @@ import { honoClient } from "@/components/fetcher";
 import { i18n } from "@/components/i18n";
 import { SafeAreaView, Text } from "@/components/injector";
 import { LoadingScreen } from "@/components/ui/Screens";
+import { StripeProvider } from "@stripe/stripe-react-native";
 import { useQuery } from "@tanstack/react-query";
 
 const Premium = () => {
@@ -20,15 +21,21 @@ const Premium = () => {
   if (!session) return null;
 
   return (
-    <SafeAreaView className="flex flex-1 flex-col gap-3 bg-background p-4">
-      <Text className="font-extrabold text-4xl">
-        {i18n.t("account.premium.title")}
-      </Text>
+    <StripeProvider
+      publishableKey={process.env.EXPO_PUBLIC_STRIPE as string}
+      urlScheme="nexttravel"
+      merchantIdentifier="app.nexttravel"
+    >
+      <SafeAreaView className="flex flex-1 flex-col gap-3 bg-background p-4">
+        <Text className="font-extrabold text-4xl">
+          {i18n.t("account.premium.title")}
+        </Text>
 
-      {plans?.map((plan) => (
-        <PlanCard key={plan.id} plan={plan} />
-      ))}
-    </SafeAreaView>
+        {plans?.map((plan) => (
+          <PlanCard key={plan.id} plan={plan} />
+        ))}
+      </SafeAreaView>
+    </StripeProvider>
   );
 };
 
